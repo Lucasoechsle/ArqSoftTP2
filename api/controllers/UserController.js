58a8e259e793b3a23091f4983d7a07dc4d2b1531
Ricardo Martín Marcucci <rmarku@gmail.com>
2021-06-08 17:54:04 -0300

    Added sms to login

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,28 +1,30 @@
 module.exports = {
   login: async function (req, res) {
-    const email = req.param('email');
-    const contra = req.param('contrasenia');
+    const email = req.param('email')
+    const contra = req.param('contrasenia')
 
     const user = await User.findOne({
       email: email,
       password: contra,
-    });
+    })
     if (user) {
-      req.session.user = user;
-      res.redirect("/");
+      req.session.user = user
+      res.redirect('/')
 
     } else {
-      req.session.user = null;
-      res.redirect("/");
+      req.session.user = null
+      res.view('pages/login', { msg: 'Nombre de usuario o contraseña incorrecto' })
 
     }
   },
 
-    logout: async function (req, res) {
-      req.session.user = null;
-      res.redirect('/');
-    }
+  logout: async function (req, res) {
+    req.session.user = null
+    res.redirect('/')
+  },
+  showLogin:async function (req, res) {
+    res.view('pages/login',{msg:null})
+  },
 
-
-};
+}
 
